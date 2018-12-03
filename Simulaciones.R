b25e756a9e56848264e5f2b562ca8622c275695a
Edgar Quiroz <equiroz@ciencias.unam.mx>
2018-12-02 19:59:24 -0600

    Añadiendo la distribución Gamma

--- Simulaciones.R
+++ Simulaciones.R
@@ -1,24 +1,27 @@
-## Tarea de Simulaciones en R
+# Tarea de Simulaciones en R
+# Simulaciones de variables aleatorias en R
 
-## Integrantes:
-## Alan Ernesto Arteaga Vázquez
-## Raúl Llamosas Alvarado
-## Edgar Quiroz Castañeda
-## Jean Paul Ruiz Melo
-## Sandra Del Mar Soto Corderi
-##
+# Integrantes:
+# Alan Ernesto Arteaga Vázquez
+# Raúl Llamosas Alvarado
+# Edgar Quiroz Castañeda
+# Jean Paul Ruiz Melo
+# Sandra Del Mar Soto Corderi
+#
 
-##1. Realice un programa que simule M valores de la distribución:
+library(gridExtra)
+library(R2HTML)
 
-##declarar un vector
+
+#declarar un vector
 y<-c(1,2,3,4)
-x<-c("a,b,c,d") 
-##vector vacio
+x<-c("a,b,c,d")
+#vector vacio
 x<-c()
 
-##a) HiperGeo(N, m, n)
+# HiperGeo(N, m, n)
 
-##b) BinNeg(n, p)
+# BinNeg(n, p)
 
 simula_BinNeg<-function(m, n, p){
   x = rnbinom(m, n, p)
@@ -26,10 +29,41 @@ simula_BinNeg<-function(m, n, p){
 }
 
 #Ejemplo
-simula_BinNeg(5, 6, 0.5)
+#simula_BinNeg(5, 6, 0.5)
 
 #Gráfica de la simulacion
 
-##c) Poisson(λ)
+# Poisson(λ)
 
-##d) Gamma(n, λ)
+# Gamma(n, λ)
+simula_Gamma <- function(n, lambda){
+    u = prod(runif(n, 0, 1))
+    x = -(1/(lambda)) * log(u)
+    return(x)
+}
+
+#Main
+#Revisando el valor de M
+args = commandArgs(trailingOnly=TRUE)
+if(length(args)==0){
+    stop("Uso: simulación.R M")
+}
+m = as.integer(args[1])
+if(is.na(m)){
+    stop("Valor no numérico")
+}
+if(m < 0) {
+    stop("Valor negativo inválidos")
+}
+
+#Generando Gamma
+n = 5
+lambda = 1
+randX = numeric(m)
+
+for(i in 1:m){
+    g = simula_Gamma(n, lambda)
+    randX[i] = g
+}
+title = sprintf("Gamma with n=%1.1f, lambda=%1.1f", n, lambda)
+hist(randX, main=title, breaks=m/5)
