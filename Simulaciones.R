642d31c89bb4d8fdb1573753d9d11252181ca56d
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-12-03 00:01:51 -0600

    Añadiendo la distribución hipergeométrica

--- Simulaciones.R
+++ Simulaciones.R
@@ -9,6 +9,32 @@
 # Sandra Del Mar Soto Corderi
 
 # HiperGeo(N, m, n)
+simula_Hiper<-function(N, n, m){
+    A = m
+    B = N - m
+    commonComb = dim(combn(N,n))[2]
+    u = runif(1, 0, 1)
+    pointsVector = vector(mode = "numeric", length = n)
+    for (i in 0:n){
+        if (i > A || (n - i) > B){
+            pointsVector[i + 1] = 0
+        } else {
+            pointsVector[i + 1] = (dim(combn(A,i))[2] * dim(combn(B,n - i))[2]) / commonComb
+        }
+        if (i != 0){
+            pointsVector[i + 1] = pointsVector[i + 1] + pointsVector[i]
+        }
+    }
+    if(u < pointsVector[1]){
+        return(0)
+    }
+    for (j in seq_along(pointsVector)){
+        if (u > pointsVector[j] && u < pointsVector[j + 1] ){
+            return(j)
+        }
+    }
+    
+}
 
 # BinNeg(n, p)
 simula_BinNeg<-function(n, p){
@@ -39,6 +65,22 @@ if(is.na(m)){
 if(m < 0) {
     stop("Valor negativo inválido")
 }
+
+# Generando Hipergeométrica
+N = 13
+n = 4
+A = 7
+
+randX = numeric(m)
+
+for(i in 1:m){
+    hyper = simula_Hiper(N, n, A)
+    randX[i] = hyper
+}
+print("Números aleatorios con distribución hipergeométrica generados")
+print(randX)
+title = sprintf("Hipergeométrica con N=%1.1f, n=%1.2f, m=%1.2f ", N, n, m)
+hist(randX, main=title, breaks = m/n)
 
 # Generando Binomial Negativa
 n = 20
