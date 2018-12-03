63c609172268633e601eb58cf6bb03c7020234b6
SandradelMarSoto <sandradelmarsoto@ciencias.unam.mx>
2018-12-02 19:18:06 -0600

    cambios

--- Simulaciones.R
+++ Simulaciones.R
@@ -10,25 +10,29 @@
 
 ##1. Realice un programa que simule M valores de la distribución:
 
-##declarar un vector
-y<-c(1,2,3,4)
-x<-c("a,b,c,d") 
-##vector vacio
-x<-c()
-
 ##a) HiperGeo(N, m, n)
 
 ##b) BinNeg(n, p)
 
+#Para simular esta función tomamos el método de la transformación inversa de n variables
+#geometricas,
 simula_BinNeg<-function(m, n, p){
-  x = rnbinom(m, n, p)
+  x<-c()
+  u<-runif(m + n)
+  for(i in 1:m){
+    for(j in 1:n){
+      x[i]= log(u[i])/log(1-p)
+    }
+  }
   return(x)
 }
 
 #Ejemplo
-simula_BinNeg(5, 6, 0.5)
+s <- simula_BinNeg (6, 5, 0.5)
+rnbinom(6, 5, 0.5)
 
 #Gráfica de la simulacion
+hist(s)
 
 ##c) Poisson(λ)
 
